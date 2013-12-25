a53352b5d3d57db6a4756c8b3626a6e799447e2a
Robert Russes <russes@mac.com>
2013-12-25 17:51:31 -0500

    cleaned up versions of Jeff’s original ReverseGeocoder.[hm] files should work with iOS 7 and ARC

--- ReverseGeocoder/ReverseGeocoder.h
+++ ReverseGeocoder/ReverseGeocoder.h
@@ -1,52 +1,67 @@
 //
-//  ReverseGeocoder.h
-//  ReverseGeocoder
-//
-//  Created by Jeff Grimes on 8/20/12.
-//  Copyright (c) 2012 Jeff Grimes. All rights reserved.
+//  ReverseGeocoder.h - ReverseGeocoder
+//  Created by Jeff Grimes on 20-Aug-12 - Copyright (c) 2012 Jeff Grimes - All Rights Reserved
 //
 
-#import <Foundation/Foundation.h>
+#import     <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
-#import <AddressBook/ABPerson.h>
+#import      <AddressBook/ABPerson.h>
+
+// -- http://www.cimgf.com/2010/05/02/my-current-prefix-pch-file --
+
+#define then
+
+#ifdef DEBUG
+    #define DLog(...) NSLog (@"%s %@", __PRETTY_FUNCTION__, [NSString stringWithFormat: __VA_ARGS__])
+    #define ALog(...) [[NSAssertionHandler currentHandler] handleFailureInFunction: [NSString stringWithCString: __PRETTY_FUNCTION__ encoding: NSUTF8StringEncoding] \
+                                                                              file: [NSString stringWithCString: __FILE__            encoding: NSUTF8StringEncoding] \
+                                                                        lineNumber: __LINE__  description: __VA_ARGS__]
+#else
+    #ifndef NS_BLOCK_ASSERTIONS
+    #define NS_BLOCK_ASSERTIONS
+    #endif
+    #define DLog(...) do { } while (0)
+    #define ALog(...) NSLog (@"%s %@", __PRETTY_FUNCTION__, [NSString stringWithFormat: __VA_ARGS__])
+#endif
 
 @protocol GeocoderProtocol <NSObject>
-- (void)gotUserLocation;
-- (void)gotUserLocationError;
-- (void)gotUserPlacemarks;
-- (void)gotUserPlacemarksError;
-- (void)locationServicesDisabled;
+- (void)          gotUserLocation;
+- (void)     gotUserLocationError;
+- (void)        gotUserPlacemarks;
+- (void)   gotUserPlacemarksError;
+- (void) locationServicesDisabled;
 @end
 
-@interface ReverseGeocoder : NSObject <CLLocationManagerDelegate> {
+@interface ReverseGeocoder : NSObject <CLLocationManagerDelegate>
+{
 @private
     BOOL delegateHasPlacemarks;
 }
 
-@property (nonatomic, weak) id <GeocoderProtocol> geocoderDelegate;
-@property (nonatomic, strong) CLGeocoder *geocoder;
-@property (nonatomic, strong) CLLocationManager *locationManager;
-@property (nonatomic, strong) NSTimer *locationTimer;
-@property (nonatomic, strong) NSTimer *geocodingTimer;
-@property (nonatomic, assign) float latitude;
-@property (nonatomic, assign) float longitude;
-@property (nonatomic, strong) NSString *address;
-@property (nonatomic, strong) NSString *city;
-@property (nonatomic, strong) NSString *state;
-@property (nonatomic, strong) NSString *zipcode;
-@property (nonatomic, strong) NSString *country;
-@property (nonatomic, strong) NSString *administrativeArea;
-@property (nonatomic, strong) NSString *subAdministrativeArea;
-@property (nonatomic, strong) NSString *locality;
+@property (nonatomic,   weak) id <GeocoderProtocol>       geocoderDelegate;
+@property (nonatomic, strong)            CLGeocoder              *geocoder;
+@property (nonatomic, strong)     CLLocationManager       *locationManager;
+@property (nonatomic, strong)               NSTimer         *locationTimer;
+@property (nonatomic, strong)               NSTimer        *geocodingTimer;
+@property (nonatomic, assign)                 float               latitude;
+@property (nonatomic, assign)                 float              longitude;
+@property (nonatomic, strong)              NSString               *address;
+@property (nonatomic, strong)              NSString                  *city;
+@property (nonatomic, strong)              NSString                 *state;
+@property (nonatomic, strong)              NSString               *zipcode;
+@property (nonatomic, strong)              NSString               *country;
+@property (nonatomic, strong)              NSString    *administrativeArea;
+@property (nonatomic, strong)              NSString *subAdministrativeArea;
+@property (nonatomic, strong)              NSString              *locality;
 
-+ (BOOL)areLocationServicesEnabled;
++ (BOOL) areLocationServicesEnabled;
 
-- (CLAuthorizationStatus)getAuthStatus;
-- (void)reverseGeocode;
-- (void)stopActivity;
+- (CLAuthorizationStatus) getAuthStatus;
+- (void) reverseGeocode;
+- (void) stopActivity;
 
-- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation;
-- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error;
-- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status;
+- (void) locationManager: (CLLocationManager *) manager didUpdateToLocation: (CLLocation *) newLocation fromLocation: (CLLocation *) oldLocation;
+- (void) locationManager: (CLLocationManager *) manager didFailWithError: (NSError *) error;
+- (void) locationManager: (CLLocationManager *) manager didChangeAuthorizationStatus: (CLAuthorizationStatus) status;
 
 @end
--- ReverseGeocoder/ReverseGeocoder.m
+++ ReverseGeocoder/ReverseGeocoder.m
@@ -1,162 +1,169 @@
 //
-//  ReverseGeocoder.m
-//  ReverseGeocoder
-//
-//  Created by Jeff Grimes on 8/20/12.
-//  Copyright (c) 2012 Jeff Grimes. All rights reserved.
+//  ReverseGeocoder.m - ReverseGeocoder
+//  Created by Jeff Grimes on 20-Aug-12 - Copyright (c) 2012 Jeff Grimes - All Rights Reserved
 //
 
 #import "ReverseGeocoder.h"
 
+const int  locationTimeLimit =  8;   // max time limit for getting location
+const int geocodingTimeLimit =  8;   // max time limit for reverse-geocoding location
+const int  staleCacheSeconds =  8;   // the lower this value, the pickier you are about cached location data - typical value is between 5 and 10
+
 @implementation ReverseGeocoder
 
 static ReverseGeocoder *sharedInstance = nil;
-const int locationTimeLimit = 8;  // max time limit for getting location
-const int geocodingTimeLimit = 8; // max time limit for reverse-geocoding location
-const int staleCacheSeconds = 5;  // the lower this value, the pickier you are about cached location data. a typical value is between 5 and 10.
 
-+ (id)sharedInstance {
-    @synchronized([ReverseGeocoder class]) {
-        if (sharedInstance == nil) {
-            sharedInstance = [[ReverseGeocoder alloc] init];
-        }
++ (id) sharedInstance
+{
+    @synchronized([ReverseGeocoder class])
+    {
+        if (sharedInstance == nil) sharedInstance = [[ReverseGeocoder alloc] init];
     }
     return sharedInstance;
 }
 
-+ (BOOL)areLocationServicesEnabled {
-    return [CLLocationManager locationServicesEnabled];
-}
++ (BOOL) areLocationServicesEnabled { return [CLLocationManager locationServicesEnabled]; }
 
-- (id)init {
-    if (self = [super init]) {
-        self.geocoder = [[CLGeocoder alloc] init];
-        self.locationManager = [[CLLocationManager alloc] init];
-        self.locationManager.delegate = self;
+- (id) init
+{
+    if (self = [super init])
+    {
+        self.geocoder                        = [[CLGeocoder alloc] init];
+        self.locationManager                 = [[CLLocationManager alloc] init];
+        self.locationManager.delegate        = self;
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-        self.locationTimer = [[NSTimer alloc] init];
-        self.geocodingTimer = [[NSTimer alloc] init];
+        self.locationTimer                   = [[NSTimer alloc] init];
+        self.geocodingTimer                  = [[NSTimer alloc] init];
+
         delegateHasPlacemarks = NO;
     }
     return self;
 }
 
-- (void)callGotUserLocation {
-    if ([self.geocoderDelegate respondsToSelector:@selector(gotUserLocation)]) {
-        [self.geocoderDelegate gotUserLocation];
-    } else {
-        NSLog(@"delegate does not implement gotUserLocation");
-    }
+- (void) callGotUserLocation
+{
+    if ([self.geocoderDelegate respondsToSelector: @selector(gotUserLocation)])
+        then [self.geocoderDelegate gotUserLocation];
+        else DLog (@"-- delegate doesn't implement gotUserLocation --");
 }
 
-- (void)callGotUserLocationError {
-    if (delegateHasPlacemarks) {
-        return;
-    }
-    if ([self.geocoderDelegate respondsToSelector:@selector(gotUserLocationError)]) {
-        [self.geocoderDelegate gotUserLocationError];
-    } else {
-        NSLog(@"delegate does not implement gotUserLocationError");
-    }
+- (void) callGotUserLocationError
+{
+    if (delegateHasPlacemarks) return;
+
+    if ([self.geocoderDelegate respondsToSelector: @selector(gotUserLocationError)])
+        then [self.geocoderDelegate gotUserLocationError];
+        else DLog (@"-- delegate doesn't implement gotUserLocationError --");
 }
 
-- (void)callGotUserPlacemarks {
-    if ([self.geocoderDelegate respondsToSelector:@selector(gotUserPlacemarks)]) {
-        [self.geocoderDelegate gotUserPlacemarks];
-    } else {
-        NSLog(@"delegate does not implement gotUserPlacemarks");
-    }
+- (void) callGotUserPlacemarks
+{
+    if ([self.geocoderDelegate respondsToSelector: @selector(gotUserPlacemarks)])
+        then [self.geocoderDelegate gotUserPlacemarks];
+        else DLog (@"-- delegate doesn't implement gotUserPlacemarks --");
 }
 
-- (void)callGotUserPlacemarksError {
-    if (delegateHasPlacemarks) {
-        return;
-    }
-    if ([self.geocoderDelegate respondsToSelector:@selector(gotUserPlacemarksError)]) {
-        [self.geocoderDelegate gotUserPlacemarksError];
-    } else {
-        NSLog(@"delegate does not implement gotUserPlacemarksError");
-    }
+- (void) callGotUserPlacemarksError
+{
+    if (delegateHasPlacemarks) return;
+
+    if ([self.geocoderDelegate respondsToSelector: @selector(gotUserPlacemarksError)])
+        then [self.geocoderDelegate gotUserPlacemarksError];
+        else DLog (@"-- delegate does not implement gotUserPlacemarksError --");
 }
 
-- (void)callLocationServicesDisabled {
-    if ([self.geocoderDelegate respondsToSelector:@selector(locationServicesDisabled)]) {
-        [self.geocoderDelegate locationServicesDisabled];
-    } else {
-        NSLog(@"delegate does not implement locationServicesDisabled");
-    }
+- (void) callLocationServicesDisabled
+{
+    if ([self.geocoderDelegate respondsToSelector: @selector(locationServicesDisabled)])
+        then [self.geocoderDelegate locationServicesDisabled];
+        else DLog (@"-- delegate does not implement locationServicesDisabled --");
 }
 
-- (CLAuthorizationStatus)getAuthStatus {
-    return [CLLocationManager authorizationStatus];
-}
+- (CLAuthorizationStatus)  getAuthStatus { return [CLLocationManager authorizationStatus]; }
+- (                 void) reverseGeocode {   [self.locationManager startUpdatingLocation]; }
 
-- (void)reverseGeocode {
-    [self.locationManager startUpdatingLocation];
-}
-
-- (void)stopActivity {
+- (void) stopActivity
+{
     delegateHasPlacemarks = YES;
     [self.locationManager stopUpdatingLocation];
 }
 
-- (void)geocodeLocation:(CLLocation *)location {
-    self.geocodingTimer = [NSTimer scheduledTimerWithTimeInterval:geocodingTimeLimit target:self selector:@selector(callGotUserPlacemarksError) userInfo:nil repeats:NO];
+- (void) geocodeLocation: (CLLocation *) location
+{
+    self.geocodingTimer = [NSTimer scheduledTimerWithTimeInterval: geocodingTimeLimit target: self selector: @selector(callGotUserPlacemarksError) userInfo: nil repeats: NO];
+
     [self.geocoder reverseGeocodeLocation: location completionHandler:
-     ^(NSArray *placemarks, NSError *error) {
+
+     ^(NSArray *placemarks, NSError *error)
+     {
          [self.geocodingTimer invalidate];
-         if (error == nil) {
+         if (error == nil)
+         {
              CLPlacemark *placemark = placemarks[0];
              
-             self.address = [placemark addressDictionary][(NSString *)kABPersonAddressStreetKey];
-             self.city = [placemark addressDictionary][(NSString *)kABPersonAddressCityKey];
-             self.state = [placemark addressDictionary][(NSString *)kABPersonAddressStateKey];
+             self.address = [placemark addressDictionary][(NSString *) kABPersonAddressStreetKey];
+             self.city    = [placemark addressDictionary][(NSString *)   kABPersonAddressCityKey];
+             self.state   = [placemark addressDictionary][(NSString *)  kABPersonAddressStateKey];
              self.zipcode = [placemark postalCode];
-             self.country = [placemark country];
-             // you can also use the same format to fetch administrative area, sub-administrative area, and locality.
+             self.country = [placemark    country];
+ 
+// -- You can also use the same format to fetch administrative area, sub-administrative area, and locality. --
              
              [self callGotUserPlacemarks];
-         } else {
-             [self callGotUserPlacemarksError];
-         }
-     }];
+         } else [self callGotUserPlacemarksError];
+     }                  ];              // end of method call
 }
 
-- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
-    NSDate *eventDate = newLocation.timestamp;
-    NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
-    BOOL isLocationFresh = abs(howRecent) < staleCacheSeconds;
-    if (isLocationFresh) {
+- (void)locationManager: (CLLocationManager *) manager didUpdateToLocation: (CLLocation *) newLocation fromLocation: (CLLocation *) oldLocation
+{
+            NSDate      *eventDate = newLocation.timestamp;
+    NSTimeInterval       howRecent = [eventDate timeIntervalSinceNow];
+              BOOL isLocationFresh = abs(howRecent) < staleCacheSeconds;
+
+    if (isLocationFresh)
+    {
         [self.locationManager stopUpdatingLocation];
-        [self.locationTimer invalidate];
-        self.latitude = self.locationManager.location.coordinate.latitude;
+        [self.locationTimer             invalidate];
+        
+        self.latitude  = self.locationManager.location.coordinate.latitude;
         self.longitude = self.locationManager.location.coordinate.longitude;
-        [self geocodeLocation:self.locationManager.location];
-        NSLog(@"obtained fresh location");
+        
+        [self geocodeLocation: self.locationManager.location];
+        DLog(@"-- obtained fresh location --");
     }
 }
 
-- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
-    NSLog(@"location manager failed");
-    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied) {
-        [self callLocationServicesDisabled];
-    } else {
-        [self callGotUserLocationError];
-    }
+- (void) locationManager: (CLLocationManager *) manager didFailWithError: (NSError *) error
+{
+    DLog(@"-- location manager failed --");
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied)
+        then [self callLocationServicesDisabled];
+        else [self     callGotUserLocationError];
 }
 
-- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
-    if (status == kCLAuthorizationStatusDenied) {
-        NSLog(@"user disabled location services");
-        [self callLocationServicesDisabled];
-    } else if (status == kCLAuthorizationStatusAuthorized) {
-        NSLog(@"user enabled location services");
-        self.locationTimer = [NSTimer scheduledTimerWithTimeInterval:locationTimeLimit target:self selector:@selector(callGotUserLocationError) userInfo:nil repeats:NO];
-    }
+- (void) locationManager: (CLLocationManager *) manager didChangeAuthorizationStatus: (CLAuthorizationStatus) status
+{
+    if (status == kCLAuthorizationStatusDenied)
+        {
+            DLog(@"-- user disabled location services --");
+            [self callLocationServicesDisabled];
+            return;
+        }
+    
+    if (status == kCLAuthorizationStatusAuthorized)
+        {
+            DLog(@"-- user enabled location services --");
+            self.locationTimer = [NSTimer scheduledTimerWithTimeInterval: locationTimeLimit target: self selector: @selector(callGotUserLocationError) userInfo: nil repeats: NO];
+            return;
+        }
 }
 
-- (void)dealloc {
+#pragma mark - Memory Management
+
+- (void) dealloc
+{
     self.geocoderDelegate = nil;
+    DLog (@"-- ReverseGeocoder - dealloc entered --");
 }
 
 @end
